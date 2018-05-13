55bc29f7b50866aea4f682b43a0826314a3c8ca6
Pamela Chong <pjchong91@gmail.com>
2018-05-13 01:26:05 -0700

    Functional start menu to player control and rules

--- src/index.js
+++ src/index.js
@@ -7,17 +7,38 @@ const game = new Game('game', 512, 256);
 
 // game.render();
 
-console.log('what is jesus');
+// console.log('what is jesus');
 let startMenu = document.getElementById('startMenu');
 let selected = startMenu.options[startMenu.selectedIndex].value;
-console.log(selected);
+// console.log(selected);
+
+let titleScreen = document.getElementById('titleScreen');
+
+let offStart = false; 
+let controls = document.getElementById('controls');
+let rules = document.getElementById('rules');
+
+//Press 'enter' off of start page to return to start
+// console.log(titleScreen);
+
+// let realH1 = document.getElementById('realH1');
+//             console.log(realH1);
 // startMenu.onsubmit = function() {
 //     console.log('what the pizza')
 // }
 
 document.addEventListener('keydown', event => {
     let selected = startMenu.options[startMenu.selectedIndex].value; //Somehow got the right variable but don't know how.
+    console.log(selected, 'SELECTED');
     if(event.keyCode === 13) {
+        if (offStart===true){
+            controls.style.display='none';
+            rules.style.display='none';
+            console.log('buttness')
+            titleScreen.style.display="flex";
+            offStart = !offStart;
+            return;
+        }
         if (selected === 'start'){
             let titleScreen = document.getElementById('titleScreen');
             titleScreen.style.display="none";
@@ -30,7 +51,23 @@ document.addEventListener('keydown', event => {
                 requestAnimationFrame(gameLoop);
              })();
 
-        } else {
+        } else if (selected === 'playerControls') {
+            let controls = document.getElementById('controls');
+            controls.style.display="flex";
+           
+            let titleScreen = document.getElementById('titleScreen');
+            console.log(titleScreen);
+            offStart = !offStart;
+            titleScreen.style.display="none";
+            console.log('yo mama fat')
+            console.log(offStart);
+        } else if (selected === 'rules') {
+            let rules = document.getElementById('rules');
+            rules.style.display="flex";
+            // console.log(titleScreen);
+            let titleScreen = document.getElementById('titleScreen');
+            offStart = !offStart;
+            titleScreen.style.display="none";
             console.log('yo mama fat')
         }
            
